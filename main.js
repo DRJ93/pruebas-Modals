4dc4669d5b1dac05a1c9ea9729f07b9fe32dd747
DRJ93 <129706028+DRJ93@users.noreply.github.com>
2024-11-12 15:17:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function abrirModal(id){
+    modal = document.getElementById(id).style= 'display:block';
+}
+function cerrarModal(id){
+    modal = document.getElementById(id).style= 'display:none';
+
+}
+
